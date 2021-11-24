7a913918780109be28475a908a06c62e9eff7a9b
sim <dhksdn4689@naver.com>
2021-11-24 23:37:15 +0900

    first commit!!!

---
+++ mainapp/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path
+
+import mainapp
+from mainapp.views import main
+
+app_name = 'mainapp'
+
+urlpatterns = [
+    path('', main, name='main'),
+]
