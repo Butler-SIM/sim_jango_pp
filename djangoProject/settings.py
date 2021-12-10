8a62c10f5895778c1ecefe141d05798763446e8b
simwanwoo <dhksdn4689@naver.com>
2021-12-10 12:00:54 +0900

    rds 추가 깃 이그노어 추가

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -78,10 +78,10 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django_portfolio',
-        'USER': 'root',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
+        'NAME': 'butler-sim',
+        'USER': 'admin',
+        'PASSWORD': 'qwer1234',
+        'HOST': 'butlersimdb.cypn7ittsrwd.ap-northeast-2.rds.amazonaws.com',
         'PORT': '3306',
         "OPTIONS": {"charset": "utf8mb4"},
     }
