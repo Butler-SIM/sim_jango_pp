8dba28652bffe041131236a0ecd430f31fb86a39
simwanwoo <dhksdn4689@naver.com>
2022-02-17 16:30:24 +0900

    환경설정 분리 test

--- server_settings/settings/local.py
+++ server_settings/settings/local.py
@@ -1,20 +1,12 @@
 from .base import *
 
-SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-secret-key')
-algorithm = os.environ.get('algorithm')
 
 DEBUG = True
-ALLOWED_HOSTS = ['*']
-
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('APP_DB_ENGINE'),
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
-        }
-     }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
