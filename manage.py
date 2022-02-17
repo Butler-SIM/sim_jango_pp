501152d7c6b55b100ec5337eabb830df11e1c479
simwanwoo <dhksdn4689@naver.com>
2022-02-17 18:18:57 +0900

    manage.py local로 실행시 예외처리, footer 수정

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server_settings.settings.local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server_settings.settings.deploy')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
