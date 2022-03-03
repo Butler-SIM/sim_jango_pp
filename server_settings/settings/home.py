83587aa199892cdd6026d0f83910bad6e5d5c32a
sim <dhksdn4689@naver.com>
2022-03-03 23:12:07 +0900

    .

---
+++ server_settings/settings/home.py
@@ -0,0 +1,27 @@
+from .base import *
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-secret-key')
+algorithm = os.environ.get('algorithm')
+
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+
+SECRET_KEY = 'django-insecure-t_9znj@bw+-cbx2nkdwlbihf3woilscsbm+t!=ze$5!sqp3=5w'
+algorithm='HS256'
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+#DB
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'butler-sim',
+        'USER': 'admin',
+        'PASSWORD': 'qwer1234',
+        'HOST': 'butlersimdb.cypn7ittsrwd.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '3306',
+        "OPTIONS": {"charset": "utf8mb4"},
+    }
+}
+
