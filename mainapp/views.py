f26d366092b5a31ac1f6841dd770f52716578a84
sim <dhksdn4689@naver.com>
2022-03-10 23:55:10 +0900

    .

--- mainapp/views.py
+++ mainapp/views.py
@@ -10,11 +10,14 @@ def main(request):
 
     return render(request, 'index.html')
 
+
 def about(request):
     return render(request, 'about.html')
 
+
 def contact(request):
     return render(request, 'contact.html')
+
 
 def portfolio(request):
     web_list = ''
@@ -31,6 +34,7 @@ def portfolio(request):
     model = {"web_list" : web_list, "app_list" : app_list}
 
     return render(request, 'portfolio.html',model)
+
 
 def portfolio_details(request):
     id = int(request.GET['id'])
@@ -57,8 +61,10 @@ def portfolio_details(request):
 
     return render(request, 'portfolio-details.html',model)
 
+
 def resume(request):
     return render(request, 'resume.html')
+
 
 def services(request):
     return render(request, 'services.html')
